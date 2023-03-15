7cb7b68f9541bb8ab99307657cb8823122a3ddfa
Omar A <omar.abdelalim@stryker.com>
2023-03-15 14:28:40 -0600

    login, signup, and logout enhancements

--- app.py
+++ app.py
@@ -1,40 +1,68 @@
-from flask import Flask, render_template, request, redirect, url_for
+import os
+from flask import Flask, render_template, redirect, url_for, request, flash
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, LoginManager, login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Vhrmonh925@localhost/postgres'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'a secret key'
+app.config['SECRET_KEY'] = os.urandom(24)
 db = SQLAlchemy(app)
 
-class User(db.Model):
+# Initialize the login manager
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=False)
 
     def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
+        self.password = generate_password_hash(password)
 
     def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
+        return check_password_hash(self.password, password)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 @app.route('/')
+def index():
+    if not current_user.is_authenticated:
+        return redirect(url_for('signup'))
+    return redirect(url_for('home'))
+
+@app.route('/home')
+@login_required
 def home():
     return render_template('home.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and user.check_password(password):
+            login_user(user)
+            return redirect(url_for('home'))
+        else:
+            flash('Invalid username or password.', 'error')
+    return render_template('login.html')
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        if len(password) < 4:
-            error_message = 'Password should be at least 4 characters long.'
-            return render_template('signup.html', error_message=error_message)
         existing_user = User.query.filter_by(username=username).first()
         if existing_user:
-            error_message = 'Username already exists. Please choose a different one.'
-            return render_template('signup.html', error_message=error_message)
+            flash('Username already exists. Please choose a different one.', 'error')
+            return render_template('signup.html')
         new_user = User(username=username)
         new_user.set_password(password)
         db.session.add(new_user)
@@ -42,20 +70,11 @@ def signup():
         return redirect(url_for('login'))
     return render_template('signup.html')
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        user = User.query.filter_by(username=username).first()
-        if user and user.check_password(password):
-            # Login successful
-            return redirect(url_for('home'))
-        else:
-            # Login failed
-            error_message = 'Invalid username or password. Please try again.'
-            return render_template('login.html', error_message=error_message)
-    return render_template('login.html')
+@app.route('/logout', methods=['POST'])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
 
 if __name__ == '__main__':
     app.run(port=3000, debug=True)
